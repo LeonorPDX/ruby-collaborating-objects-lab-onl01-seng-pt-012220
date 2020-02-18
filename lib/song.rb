c4b3e58c221bbf300de6f316fe4d0ce6c8d951bb
Leonor Colbert <leonoracolbert@gmail.com>
2020-02-18 19:52:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
-    song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    song.artist_name(filename.split(" - ")[0])
     song
   end
   
