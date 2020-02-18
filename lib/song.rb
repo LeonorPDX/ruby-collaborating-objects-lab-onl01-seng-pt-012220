2c1213332b18b638d83d159d88299ed7fc3f94a5
Leonor Colbert <leonoracolbert@gmail.com>
2020-02-18 20:00:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
     song_name = filename.split(" - ")[1])
     song_artist = filename.split(" - ")[0])
     song = Song.new(song_name)
-    song.artist_name(song_artist)
+    song.artist_name=(song_artist)
     song
   end
   
