073bf68d563427f3b86dc716083ec1843c99dd39
Leonor Colbert <leonoracolbert@gmail.com>
2020-02-18 19:41:58 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -12,8 +12,7 @@ class MP3Importer
   end
   
   def import
-    @files.each do |filename|
-      Song.new_by_filename(filename)
+    @files.each {|filename| Song.new_by_filename(filename)}
     end
   end
   
